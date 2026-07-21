9579f59f17c4ab66cefa0e6209dc6af6a23107a5
Zacky Rafian Fawwauzy <imftcy@gmail.com>
2026-07-21 18:56:11 +0700

    feat: auth middleware Authorization

---
+++ internal/middleware/auth.go
@@ -0,0 +1,20 @@
+package middleware
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func AuthMiddleware() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		if c.GetHeader("Authorization") != "hello" {
+			c.JSON(http.StatusUnauthorized, gin.H{
+				"error": "Unauthorized",
+			})
+			c.Abort()
+			return
+		}
+		c.Next()
+	}
+}
