82ed40392d207122a112098779111de0afad4e68
Zacky Rafian Fawwauzy <imftcy@gmail.com>
2026-07-22 14:36:52 +0700

    feat(server): services editUsers, uploadProfilePicture, getUsersByID

--- internal/service/users_service.go
+++ internal/service/users_service.go
@@ -3,6 +3,7 @@ package service
 import (
 	"context"
 	"errors"
+	"fmt"
 	"koda-b8-backend1/internal/domain"
 	"koda-b8-backend1/internal/repository"
 )
@@ -26,6 +27,20 @@ func (s *UserService) GetUsers(ctx context.Context) (*[]domain.User, error) {
   return s.repository.FindAll(ctx)
 }
 
+func (s *UserService) GetUserByID(id int64, ctx context.Context) (*domain.User, error) { 
+  return s.repository.FindByID(id, ctx)
+}
+
+func (s *UserService) DeleteUser(id int64, ctx context.Context) (error) { 
+  user, err := s.repository.FindByID(id, ctx) 
+  fmt.Print(user)
+
+  if err != nil { 
+    return nil
+  }
+  return s.repository.Delete(int64(user.Id), ctx)
+}
+
 func (s *UserService) Login(data *domain.LoginRequest, ctx context.Context) (*domain.User, error) {
 	user, err := s.repository.FindByEmail(data.Email, ctx)
 	if err != nil {
@@ -35,4 +50,20 @@ func (s *UserService) Login(data *domain.LoginRequest, ctx context.Context) (*do
 		return nil, errors.New("password salah")
 	}
 	return user, nil
+}
+
+func (s *UserService) Patch(id int64, req *domain.PatchUserRequest, ctx context.Context) (*domain.User, error) { 
+  _, err := s.repository.FindByID(id, ctx)
+  if err != nil {
+    return nil, err
+  }
+  return s.repository.Patch(id, req, ctx)
+}
+
+func (s *UserService) UploadPictureProfile(id int64, req *domain.UploadPicturesProfileRequest, ctx context.Context) (*domain.User, error) {
+  _, err := s.repository.FindByID(id, ctx)
+  if err != nil {
+    return nil, err
+  }
+  return s.repository.UploadPictureProfile(id, req, ctx)
 }
