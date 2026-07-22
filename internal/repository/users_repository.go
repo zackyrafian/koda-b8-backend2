e0a28355dc4090b50382ee0d5868587a2ad2b9ad
Zacky Rafian Fawwauzy <imftcy@gmail.com>
2026-07-22 14:39:27 +0700

    feat(server): repository delete user, patch user, upload profile picture

--- internal/repository/users_repository.go
+++ internal/repository/users_repository.go
@@ -63,7 +63,7 @@ func (r *UserRepository) FindAll(ctx context.Context) (*[]domain.User, error) {
   var users []domain.User
   rows, err := r.db.Query(
     ctx, `
-    SELECT users.id as id, fullname, email FROM users LEFT JOIN user_profiles ON users.id = user_profiles.user_id
+    SELECT users.id as id, fullname, email, picture FROM users LEFT JOIN user_profiles ON users.id = user_profiles.user_id
     `, 
   )
 
@@ -72,7 +72,7 @@ func (r *UserRepository) FindAll(ctx context.Context) (*[]domain.User, error) {
   }
   for rows.Next() { 
     var p domain.User
-    err = rows.Scan(&p.Id, &p.FullName, &p.Email)
+    err = rows.Scan(&p.Id, &p.FullName, &p.Email, &p.Picture)
     users = append(users, p)
   }
   return &users, nil
@@ -82,9 +82,11 @@ func (r *UserRepository) FindByEmail(email string, ctx context.Context) (*domain
 	user := &domain.User{}
 	err := r.db.QueryRow(
 	  ctx,`
-			SELECT id, email, password FROM users WHERE email = $1
+			SELECT users.id as id, email, password, fullname, profile FROM users
+		  LEFT JOIN user_profiles ON users.id = user_profiles.user_id
+			WHERE email = $1
 		`, email, 
-	).Scan(&user.Id ,&user.Email, &user.Password)
+	).Scan(&user.Id ,&user.Email, &user.Password, &user.FullName)
 	if err != nil {
 		if errors.Is(err, pgx.ErrNoRows) {
 			return nil, errors.New("user tidak ditemukan")
@@ -108,4 +110,70 @@ func (r *UserRepository) FindByID(id int64, ctx context.Context) (*domain.User, 
 		return nil, err
 	}
 	return user, err
+}
+
+func (r *UserRepository) Delete(id int64, ctx context.Context) (error) {
+	err := r.db.QueryRow(
+	  ctx,`
+			DELETE FROM users WHERE id = $1
+		`, id, 
+	)
+	if err != nil { 
+	  return nil
+	}
+	return nil
+}
+
+func (r *UserRepository) Patch(id int64, req *domain.PatchUserRequest, ctx context.Context) (*domain.User, error) {
+  tx, err := r.db.Begin(ctx) 
+  if err != nil { 
+    return nil, err
+  }
+  defer tx.Rollback(ctx)
+  user := &domain.User{}
+
+  err = tx.QueryRow(
+    ctx, `
+      UPDATE users
+      SET email = COALESCE($1, email)
+      WHERE id = $2
+      RETURNING id, email 
+    `, req.Email, id, 
+  ).Scan(&user.Id, &user.Email)
+
+  if err != nil { 
+    return nil, err 
+  }
+
+  _, err = tx.Exec(
+    ctx, `
+      UPDATE user_profiles 
+      SET fullname = COALESCE($1, fullname)
+      WHERE user_id = $2
+    `, req.FullName, id,
+  )
+  if err != nil { 
+    return nil, err
+  }
+  if err := tx.Commit(ctx); err != nil { 
+    return nil, err
+  }
+  user.FullName = *req.FullName
+  return user, nil
+}
+
+func (r *UserRepository) UploadPictureProfile(id int64, req *domain.UploadPicturesProfileRequest, ctx context.Context) (*domain.User, error) { 
+    var user domain.User
+    err := r.db.QueryRow(
+      ctx,`
+        UPDATE user_profiles
+        SET picture = COALESCE($2, picture)
+        WHERE id = $1 
+        RETURNING id, picture
+      `, id, req.Picture,
+    ).Scan(&user.Id, &user.Picture)
+    if err != nil {
+      return nil, err
+    }
+    return &user , err 
 }
