152a81eb8cd6d8c4203a4e36f9d3dc94ebeda259
Zacky Rafian Fawwauzy <imftcy@gmail.com>
2026-07-23 00:28:16 +0700

    feat: add configuration swagger

--- cmd/main.go
+++ cmd/main.go
@@ -4,15 +4,33 @@ import (
 	"context"
 	"fmt"
 	"koda-b8-backend1/internal/di"
+	"koda-b8-backend1/internal/middleware"
+	_ "koda-b8-backend1/docs"
 	"os"
 
 	"github.com/gin-gonic/gin"
 	"github.com/jackc/pgx/v5/pgxpool"
+	"github.com/joho/godotenv"
+	swaggerFile "github.com/swaggo/files"
+	ginSwagger "github.com/swaggo/gin-swagger"
 )
+
+// @title Backend V2
+// @version 1.0
+// @description API documentation
+// @host localhost:9999
+// @BasePath /
 
 func main () {
   r := gin.Default()
-  conn, err := pgxpool.New(context.Background(), "postgres://postgres:admin@localhost:5433/postgres")
+  r.GET("/docs/*any", ginSwagger.WrapHandler(swaggerFile.Handler))
+  err := godotenv.Load()
+  if err != nil { 
+    fmt.Print("Failed Load .ENV")
+  }
+  r.Static("/uploads", "./uploads")
+  r.Use(middleware.Cors())
+  conn, err := pgxpool.New(context.Background(), os.Getenv("DATABASE_URL"))
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "Unable to connect to database: %v\n", err)
 		os.Exit(1)
